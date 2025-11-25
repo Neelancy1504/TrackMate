24867590409d4f57000308c4a48655e5cbf819ca
Neelancy1504 <neelancynain0205@gmail.com>
2025-11-25 22:04:55 +0530

    Added Articles Button

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -42,9 +42,9 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="https://finance-article-page-nu.vercel.app/">
             <Button size="lg" variant="outline" className="px-8">
-              Watch Demo
+              Articles
             </Button>
           </Link>
         </div>
